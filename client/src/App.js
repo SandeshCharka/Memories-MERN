e978c0c65970fd955fd24367d4d2d3e7570892af
sandesh <sd.charka@gmail.com>
2020-11-23 05:23:11 -0800

    first-commit

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memoriesImage from "./images/memoriesImage.png";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        {/* Any textual element H2 P or anything but gives it a nice font */}
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+          <img className={classes.image} src={memoriesImage} alt="memories" height="60"></img>
+        </Typography>
+      </AppBar>
+      {/* Grow provides simple animation, property of in */}
+      <Grow in>
+        <Container>
+          {/* Grid of type container */}
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            {/* Add two different grid items within grid container */}
+            {/* Full width on extra small devices */}
+            {/* On small or medium devices will take 7/12 spaces */}
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+// Newest versions of node we can use the import syntax, more modern and easier to use.
+// const express = require('express');
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = `mongodb+srv://sandeshcharka:fseanfsean@memoriesmern.x9fhv.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  // useNewUrlParser/useUnifiedTopology not required but if not setup then may get some errors/warnings in the console
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// Makes sure we dont get any warnings in the console
+mongoose.set("useFindAndModify", false);
