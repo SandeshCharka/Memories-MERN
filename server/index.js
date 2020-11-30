3f9080ea6e2af2237289e77dd00935d9254cb85d
sandesh <sd.charka@gmail.com>
2020-11-30 04:47:46 -0800

    cors-fix

--- server/index.js
+++ server/index.js
@@ -9,13 +9,12 @@ import postRoutes from "./routes/posts.js";
 // const express = require('express');
 
 const app = express();
-console.log('hey')
-
-app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 // https://www.mongodb.com/cloud/atlas
 
